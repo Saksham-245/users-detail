7f3d13f89dfda848d795f40ce74ffe8a8e690cec
Saksham Mathur <saksham.mathur25@gmail.com>
2021-08-04 19:05:34 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react-hooks/exhaustive-deps */
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Card from "./component/Card";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const request1 = "https://reqres.in/api/users";
+
+  const fetchUserPage1 = () => {
+    axios.get(request1).then((response) => {
+      const { data } = response.data;
+      console.log(data);
+      setUsers(users,[...data]);
+    });
+  };
+
+  fetchUserPage1();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="row">
+        {users.map((user) => {
+          return (
+            <Card
+              key={user.id}
+              firstname={user.first_name}
+              lastname={user.last_name}
+              source={user.}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
